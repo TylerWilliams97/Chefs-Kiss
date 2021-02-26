9a9a9dcf505088ba0647105216329c5f9e3d8e10
Joseph <josephgrant91739@gmail.com>
2021-02-25 19:58:26 -0800

    more members.html changes

--- public/js/members.js
+++ public/js/members.js
@@ -5,14 +5,3 @@ $(document).ready(() => {
     $(".member-name").text(data.email);
   });
 });
-// Onclick function to post recipe
-const postButton = document.querySelector("#postButton")
-const 
-document.getElementById("postButton").addEventListener("click", function(){
-
-});
-
-function clickPost() {
-  document.getElementById("postButton").innerHTML = "";
-  // -----------------------------------------------^insert function above
-}
